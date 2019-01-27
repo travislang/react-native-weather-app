a6a039e9eb02446abd4622f518f752d64086c015
Travis Lang <tlang50@icloud.com>
2019-01-27 13:37:38 -0600

    add api calls to server and implement on client with fetch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,7 @@ import {
     StyleSheet,
     Text,
     TouchableOpacity,
+    ActivityIndicator,
     View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
@@ -16,6 +17,7 @@ export default class HomeScreen extends React.Component {
         header: null,
     };
     state = {
+        loading: false,
         backgroundUrl: '',
         location: {
             lat: '',
@@ -28,38 +30,20 @@ export default class HomeScreen extends React.Component {
             precipProbability: ''
         }
     }
-    componentDidMount() {
-        
-        // fetch('https://api.unsplash.com/photos/random', {
-        //     method: 'GET',
-        //     headers: {
-        //         Authorization: `Client-ID ${devEnv.DARKSKY_API}`
-        //     },
-        // })
-        //     .then(response => response.json())
-        //     .then(result => {
-        //         console.log('this is result', result);
-
-        //         this.setState({
-        //             backgroundUrl: result.urls.raw
-        //         })
-        //     })
-        //     .catch(err => {
-        //         console.log('error in get', err);
-
-        //     })
-    }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.startButtonContainer}>
-                    <TouchableOpacity onPress={this._handleStartPress}>
-                        <Text style={styles.startText}>Get Weather</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.startButtonContainer}>
-                    <Text>{this.state.location.lat}</Text>
+                    {!this.state.loading ? 
+                        <TouchableOpacity onPress={this._handleStartPress}
+                            style={styles.startButton}>
+                            <Text style={styles.startText}>Get Weather</Text>
+                        </TouchableOpacity>
+                        :
+                        <ActivityIndicator size="large" color="#83d8fc" />
+                    }
+                    
                 </View>
                 <Image
                     source={{ uri: this.state.backgroundUrl || 'https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80' }}
@@ -69,9 +53,11 @@ export default class HomeScreen extends React.Component {
     }
 
     _handleStartPress = () => {
-        // AlertIOS.alert('Sync Complete', 'All your data are belong to us.');
+        console.log('hit');
+        
         navigator.geolocation.getCurrentPosition((pos) => {
             this.setState({
+                loading: true,
                 location: {
                     lat: pos.coords.latitude,
                     lng: pos.coords.longitude
@@ -83,51 +69,43 @@ export default class HomeScreen extends React.Component {
 
     getCurrentWeather = () => {
         console.log('in getCurrentWeather');
-        console.log(this.state.location);
         fetch(`http://192.168.1.5:5000/api/weather/${this.state.location.lat}/${this.state.location.lng}`)
             .then(response => {
                 return response.json()
             })
             .then(res => {
-                // this.setState({
-                //     tempData: {
-                //         realFeel: res.currently.apparentTemperature,
-                //         temp: res.currently.temperature,
-                //         tempSummary: res.currently.icon,
-                //         precipProbability: res.currently.precipProbability
-                //     }
-                // })
-                console.log(res);
+                this.setState({
+                    tempData: {
+                        realFeel: res.currently.apparentTemperature,
+                        temp: res.currently.temperature,
+                        tempSummary: res.currently.icon,
+                        precipProbability: res.currently.precipProbability
+                    }
+                })
+                this.getWeatherImage();
             })
             .catch(err => {
-                console.log('error in darksky api call', err);
+                console.log('error getting weather data', err);
             })
 
     }
+
+    getWeatherImage = () => {
+        console.log('this is summary of current temp', this.state.tempData.tempSummary);
+        fetch(`http://192.168.1.5:5000/api/weather-image?summary=${this.state.tempData.tempSummary}`)
+            .then(response => response.json())
+            .then(result => {
+                this.setState({
+                    loading: false,
+                    backgroundUrl: result.urls.raw
+                })
+            })
+            .catch(err => {
+                console.log('error in get', err);
+
+            })
+    }
 }
-
-// "currently": Object {
-// [02:44:03]     "apparentTemperature": -23.91,
-// [02:44:03]     "cloudCover": 0,
-// [02:44:03]     "dewPoint": -18.85,
-// [02:44:03]     "humidity": 0.79,
-// [02:44:03]     "icon": "clear-night",
-// [02:44:03]     "nearestStormBearing": 221,
-// [02:44:03]     "nearestStormDistance": 153,
-// [02:44:03]     "ozone": 366.72,
-// [02:44:03]     "precipIntensity": 0,
-// [02:44:03]     "precipProbability": 0,
-// [02:44:03]     "pressure": 1024.2,
-// [02:44:03]     "summary": "Clear",
-// [02:44:03]     "temperature": -14.38,
-// [02:44:03]     "time": 1548578643,
-// [02:44:03]     "uvIndex": 0,
-// [02:44:03]     "visibility": 10,
-// [02:44:03]     "windBearing": 325,
-// [02:44:03]     "windGust": 5.84,
-// [02:44:03]     "windSpeed": 3.3,
-// [02:44:03]   },
-
 
 const styles = StyleSheet.create({
     container: {
@@ -147,8 +125,11 @@ const styles = StyleSheet.create({
     startButtonContainer: {
         zIndex: 10,
         justifyContent: 'center',
-        alignItems: 'center'
-
+        alignItems: 'center',
+    },
+    startButton: {
+        backgroundColor: 'rgba(0,0,0, 0.3)',
+        borderRadius: 15,
     },
     startText: {
         padding: 20,
@@ -156,6 +137,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         borderWidth: 2,
         borderColor: '#fff',
-        borderRadius: 15
+        borderRadius: 15,
+        
     },
 });
--- server/server.js
+++ server/server.js
@@ -10,12 +10,30 @@ const app = express();
 //PORT of server
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/weather-image', (req, res) => {
+    const queryParam = req.query.summary;
+    axios.get(`https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_API}&query=${queryParam}-weather&orientation=portrait`)
+        .then( response => {
+            res.send(response.data)
+        })
+        .catch( err => {
+            console.log('error getting image from unsplash', err);
+            
+        })
+})
+
+//gets current location weather based on lat/lng set from client
 app.get('/api/weather/:lat/:lng', (req, res) => {
     const lat = req.params.lat;
     const lng = req.params.lng;
     console.log('route hit', lat, lng);
-    axios.get(`https://api.darksky.net/forecast/${devEnv.DARKSKY_API}/${lat},${lng}?units=auto`)
-    res.json({key: 'hello'});
+    axios.get(`https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${lat},${lng}?units=auto&exclude=minutely,hourly,daily,alerts,flags`)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => {
+            console.log('error in darksky api call', err);
+        })
 })
 
 // listen for requests
