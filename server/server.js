2c6ad6f3dc435e579dc6bb5ec6f0dd4a3fd09126
Travis Lang <tlang50@icloud.com>
2019-01-27 16:41:44 -0600

    fix formatting

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.get('/api/weather-image', (req, res) => {
     const queryParam = req.query.summary;
     console.log('query', queryParam);
-    
     axios.get(`https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_API}&query=${queryParam}&orientation=portrait`)
         .then( response => {
             res.send(response.data)
